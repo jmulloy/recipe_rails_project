1690cf16f59ae9d5c85288d98a79b08f1b5532a6
github username <github email address>
2018-10-24 12:35:02 -0400

    added create method for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to new_user_path
+        end
     end
 
     
