d9251b5346e610732f01ef3abd54e417662a1fd6
github username <github email address>
2018-10-24 12:41:57 -0400

    updated show for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
     end
 
     def show
-        @recipe = Recipe.find(params[:id])
+        @recipe = Recipe.find_by(id: params[:id])
     end
 
     def destroy
