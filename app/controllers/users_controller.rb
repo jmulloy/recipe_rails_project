1531e7d922b8df11eac25c18ad012d8296b9ef02
github username <github email address>
2018-10-24 12:36:13 -0400

    fixed user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
     private
 
-    def user_params(user)
+    def user_params
         params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
