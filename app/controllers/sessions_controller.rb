657f946766515602fced4a8fb1cb6373510a2c4a
github username <github email address>
2018-10-26 09:19:12 -0400

    Added user_params to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,20 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by(email: params[:email]) 
-        if @user && @user.authenticate(params[:password])
+        @user = User.find_by(email: user_params[:email]) 
+        if @user && @user.authenticate(user_params[:password])
            session[:user_id] = @user.id 
+           binding.pry
            redirect_to user_recipes(@user)
         else
             render :new
         end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
 
    
