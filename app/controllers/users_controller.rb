5b27a9ea2a4610f71d72df24f1e019dcb0ca6a66
github username <github email address>
2018-10-29 15:06:11 -0400

    Fixed error display for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_recipes_path(@user)
         else
-            redirect_to new_user_path
+        
+            render :new
         end
     end
 
