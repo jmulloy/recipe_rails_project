a4f966e282365d328f6b08360a3b6c798de2c70e
github username <github email address>
2018-10-29 10:59:00 -0400

    Added omniauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
     validates :email, presence: true
     has_many :recipes
     has_secure_password
+
+    def self.create_with_omniauth(auth)
+        create! do |user|
+            user.provider = authp["provider"]
+            user.user_id = auth["user_id"]
+            user.name = auth["info"]["name"]
+        end
+    end
 end
