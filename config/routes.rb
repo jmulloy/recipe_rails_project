79ed8248c5ee8a9810605679f80b5c42d49151f6
github username <github email address>
2018-10-29 11:12:17 -0400

    Added proper routing for OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
+  get '/auth/github/callback' => 'sessions#create'
+
 end
