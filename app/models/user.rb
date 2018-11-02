f7c239e9bdae5151383b73e801178472163eee05
github username <github email address>
2018-11-02 11:53:26 -0400

    Commit to fix email error I recieved

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,7 @@ class User < ApplicationRecord
     def self.find_or_create_by_omniauth(auth_hash)
         self.where(:email => auth_hash["info"]["email"]).first_or_create do |user|
             user.password = SecureRandom.hex
-        # oauth_email = auth_hash["info"]["email"]
-        # if @user = User.find_by(:email => oauth_email)
-        #     return @user
-        # else
-        #     user = User.create(:email => oauth_email, :password => SecureRandom.hex)
+       
 
         end
     end
